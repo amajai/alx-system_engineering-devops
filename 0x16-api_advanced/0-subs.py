0d2abb61968ca5bf07ad7ca47ce72732a6e493d2
Abdul <aabdulmajeed.isa@gmail.com>
2024-01-17 10:24:47 +0100

    done 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,16 @@ def number_of_subscribers(subreddit):
     """
     get number of subs from a subreddit
     """
-    try:
-        url = f"https://www.reddit.com/r/{subreddit}/new.json"
-        res = requests.get(url)
-        subreddit_obj = res.json()["data"]["children"][0]["data"]
-        count = subreddit_obj["subreddit_subscribers"]
-        return count
-    except JSONDecodeError:
+    url = f"https://www.reddit.com/r/{subreddit}/new.json"
+    res = requests.get(
+        url,
+        allow_redirects=False,
+        headers={
+            "User-Agent": "MyCustomUserAgent/1.0"
+        }
+    )
+    if res.status_code == 200:
+        data = res.json()["data"]["children"][0]["data"]
+        return data['subreddit_subscribers']
+    else:
         return 0
